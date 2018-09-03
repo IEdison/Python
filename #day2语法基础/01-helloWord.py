ed8a73d93822f3b7e4d7ac2c75bff838aed8e586
IEdison <hcy1418@163.com>
2018-09-03 20:13:09 +0800

    python基础语法

---
+++ #day2语法基础/01-helloWord.py
@@ -0,0 +1,22 @@
+
+# 1.常用快捷方式：
+# ctrl + / ----单行注释
+# Ctrl + s ----保存
+# ctrl + b ----编译执行
+# ctrl + n ----新建文件
+# ctrl + c ----复制
+# ctrl + v ----粘贴
+# ctrl + z ----撤销
+# ctrl + y / ctrl +shift+z ----反撤销
+# ctrl + f ----弹出搜索框
+# 按住ctrl不放，让鼠标可以同时在多个位置设置光标
+
+# 2.print函数是python内置函数，作用是控制打印print后面()里面的任何内容。
+# ()里面的单引号，是字符串的标志，而不是内容
+# 在python2.x中 print'hello world'
+# 在python3.x中 print后面需要括号
+# A.B.C  A-->重大修改（不向下兼容）  B-->添加新的功能  C--> 修复bug
+
+print('hello world!')
+print('你好')
+
